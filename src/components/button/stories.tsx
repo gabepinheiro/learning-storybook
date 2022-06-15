e01edac7a0e497f597d0983c368a34607634f0b0
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-06-15 12:04:34 -0300

    feat(components/Button): adding stories/storybook

---
+++ src/components/button/stories.tsx
@@ -0,0 +1,39 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+
+import { Button } from './button'
+
+export default {
+  title: 'Button',
+  component: Button
+} as ComponentMeta<typeof Button>
+
+const Story: ComponentStory<typeof Button> = (args) => <Button {...args} />
+
+export const Primary = Story.bind({})
+Primary.args = {
+  children: 'Primary',
+  primary: true
+}
+
+export const Secondary = Story.bind({})
+Secondary.args = {
+  children: 'Secondary'
+}
+
+export const Small = Story.bind({})
+Small.args = {
+  children: 'Small',
+  size: 'small'
+}
+
+export const Medium = Story.bind({})
+Medium.args = {
+  children: 'Medium',
+  size: 'medium'
+}
+
+export const Large = Story.bind({})
+Large.args = {
+  children: 'Large',
+  size: 'large'
+}
